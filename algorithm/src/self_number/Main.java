deb59d8f45715591baa1586fd7802d28d2f2a083
jaesung <jaesungbong@gmail.com>
2017-01-09 16:44:55 +0900

    .

---
+++ algorithm/src/self_number/Main.java
@@ -0,0 +1,36 @@
+package self_number;
+
+import java.util.ArrayList;
+
+public class Main {
+	public int d(int num) {
+		int result = 0;
+		if ( num < 10 ) {
+			result = num * 2;
+		} else {
+			result = num;
+			while ( num != 0 ) {
+				result += num % 10;
+				num = num / 10;
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String [] args) {
+		Main main = new Main();
+		ArrayList<Integer> hasConstructor = new ArrayList<Integer>();
+		ArrayList<Integer> noConstructor = new ArrayList<Integer>();
+		int i = 1;
+		while ( i <= 10000 ) {
+			hasConstructor.add(main.d(i));
+			if (hasConstructor.indexOf(i) == -1) {
+				noConstructor.add(i);
+			}
+			i++;
+		}
+		for (int k : noConstructor) {
+			System.out.println(k);
+		}
+	}
+}
