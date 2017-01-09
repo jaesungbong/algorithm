4804bdaca92e83baecc184f26dc6bce2284a351d
jaesung <jaesungbong@gmail.com>
2017-01-09 15:54:56 +0900

    .

--- algorithm/src/dfs_bfs_adjacencylist/Main.java
+++ algorithm/src/dfs_bfs_adjacencylist/Main.java
@@ -20,6 +20,7 @@ public class Main {
             }
         }
     }
+    
     public static void bfs(ArrayList<Integer>[] a, boolean[] c, int start) {
     	int cnt = 0;
         Queue<Integer> q = new LinkedList<Integer>();
@@ -39,6 +40,7 @@ public class Main {
         System.out.println();
         System.out.println(cnt);
     }
+    
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //정점의 개수
