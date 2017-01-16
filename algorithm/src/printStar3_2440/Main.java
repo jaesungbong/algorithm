15cba0ff747cfd529a728f858a533274f56167dc
jaesung <jaesungbong@gmail.com>
2017-01-16 16:55:45 +0900

    .

---
+++ algorithm/src/printStar3_2440/Main.java
@@ -0,0 +1,22 @@
+package printStar3_2440;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			String strStar = "";
+			int gap = n - i;
+			for (int j = 0; j < gap; j++) {
+				if (j < gap) {
+					strStar += "*";
+				} else {
+					strStar += " ";
+				}
+			}
+			System.out.println(strStar);
+		}
+	}
+}
