29b12e2faaead2b864117865b9b1ea3024c09b4d
jaesung <jaesungbong@gmail.com>
2017-01-23 17:55:51 +0900

    .

---
+++ algorithm/src/maxMultiply_1500_not/Main.java
@@ -0,0 +1,20 @@
+package maxMultiply_1500_not;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+//		더하기 사이클
+//		Scanner sc = new Scanner(System.in);
+//		int s = sc.nextInt(); //정수 S
+//		int k = sc.nextInt(); //정수 K
+//		int[] nums = new int[k];
+//		int max = 0;
+//		
+//		for (int i = 0; i < k; i++) {
+//			for (int j = 0; j <= s; j++) {
+//				nums[i] = 
+//			}
+//		}
+	}
+}
---
+++ algorithm/src/numOfAdjacentBits/Main.java
@@ -0,0 +1,30 @@
+package numOfAdjacentBits;
+
+import java.util.Scanner;
+
+public class Main {
+	static int[][][] nums= new int[101][100][2]; // [a][b][c] 길이가a인 비트 중 인접한 비트의 개수가 b개이고 c로 끝나는 수열의 개수 
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		nums[1][0][1] = nums[1][0][0] = 1;
+		
+		while(t-- > 0) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			for (int i = 2; i <= 100; i++) {
+				for (int j = 0; j <= k; j++) {
+					nums[i][j][0] = nums[i - 1][j][0] + nums[i - 1][j][1];
+					if (j - 1 < 0) {
+						nums[i][j][1] = nums[i - 1][j][0];
+					} else {
+						nums[i][j][1] = nums[i - 1][j][0] + nums[i - 1][j - 1][1];
+					}
+				}
+			}
+			System.out.println(nums[n][k][0] + nums[n][k][1]);
+		}
+	}
+}
