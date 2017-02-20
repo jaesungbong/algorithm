6b5066cb855e61c9d6d8621114e97b97a9000b9c
jaesung <jaesungbong@gmail.com>
2017-02-20 16:37:41 +0900

    .

---
+++ algorithm/src/BFS_숨바꼭질_1697/Main.java
@@ -0,0 +1,45 @@
+package BFS_숨바꼭질_1697;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	static int start;
+	static int dest;
+	static int[] dist = new int[100001];
+
+	static void bfs() {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(start);
+		dist[start] = 1;
+		while (!q.isEmpty()) {
+			int x = q.remove();
+			if (x == dest) {
+				System.out.println(dist[x] - 1);
+				break;
+			} else {
+				if ((0 <= x - 1) && (dist[x - 1] == 0)) {
+					q.add(x - 1);
+					dist[x - 1] = dist[x] + 1;
+				}
+				if ((x + 1 <= 100000) && (dist[x + 1] == 0)) {
+					q.add(x + 1);
+					dist[x + 1] = dist[x] + 1;
+				}
+				if ((x * 2 <= 100000) && (dist[x * 2] == 0)) {
+					q.add(x * 2);
+					dist[x * 2] = dist[x] + 1;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		start = sc.nextInt(); // 수빈이의 위치
+		dest = sc.nextInt(); // 동생의 위치
+		bfs();
+		sc.close();
+	}
+}
